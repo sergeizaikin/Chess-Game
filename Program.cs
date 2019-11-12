57877f9384a64b79bef0458e3f35709669ae7350
Sergei Zaikin <sergeizaikin@hotmail.com>
2019-11-12 19:24:02 -0300

    Criando a classe Posicao

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using JogoXadrez.Tabuleiro;
 
 namespace JogoXadrez
 {
@@ -6,7 +7,9 @@ namespace JogoXadrez
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P = new Posicao(3, 4);
+            Console.WriteLine(P);
+
         }
     }
 }
